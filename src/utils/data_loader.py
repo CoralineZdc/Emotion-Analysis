2070c3a7efa919e1fd6d3b9ea8e2b0f44aa75e2b
Coraline Zandecki <coraline.zandecki@student-cs.fr>
2026-08-24 16:51:47 +0200

    Corrected weighted loss implementation

--- src/utils/data_loader.py
+++ src/utils/data_loader.py
@@ -159,6 +159,9 @@ class DataLoader(data.Dataset):
 
             try:
                 pixels = list(map(int, pixel_str.split()))
+                if len(pixels) != self.size * self.size:
+                    print("Warning: {} pixel string has incorrect length ({}) for {}x{} image at index {}. Skipping this entry.".format(self.split, len(pixels), self.size, self.size, idx))
+                    continue
             except ValueError:
                 print("Warning: Could not parse {} pixel string '{}' at index {}. Skipping this entry.".format(self.split, pixel_str, idx))
                 continue
